b8be1207214426aee40e4bc06b5507c7aad7c81c
Nolan Kovacik <noltron000@gmail.com>
2018-11-30 12:02:52 -0800

    creating user model/schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
+const User = require('../models/user');
 
 module.exports = (app) => {
-
 	// Get Sign-Up page
 	app.get('/sign-up', (req, res) => {
 		res.render('sign-up')
@@ -8,6 +8,15 @@ module.exports = (app) => {
 
 	// Post Sign-Up form
 	app.post('/sign-up', (req, res) => {
-
+		// instantiate instance of user model
+		const user = new User(req.body);
+		// save instance of flare model to db
+		console.log(req.body);
+		flare.save((err, flare) => {
+			// redirect to the index
+			console.log(flare);
+			console.log(err);
+			return res.redirect('/flares');
+		});
 	})
 }
